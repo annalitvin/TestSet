00e25ac0e9f2d0e8f00f7579148af5d81f901c84
annalitvin <litvin_any@ukr.net>
2020-06-16 21:11:37 +0300

    Created new django project and added user_account app

---
+++ src/user_account/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'account'
+
+urlpatterns = [
+    path('register', views.CreateUserAccountView.as_view(), name='registration'),
+    path('login', views.UserAccountLoginView.as_view(), name='login'),
+    path('logout', views.UserAccountLogoutView.as_view(), name='logout'),
+    path('profile', views.UserAccountProfileView.as_view(), name='profile'),
+
+]
