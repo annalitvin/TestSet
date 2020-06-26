7657ff327a77bac9fa7aceae6259bd0b75a17922
annalitvin <litvin_any@ukr.net>
2020-06-27 00:50:01 +0300

    Added last avr_scope in avr_scope field in User model

--- src/user_account/models.py
+++ src/user_account/models.py
@@ -6,7 +6,10 @@ from django.db import models
 
 class User(AbstractUser):
     image = models.ImageField(default='default.jpg', upload_to='pics')
-    avr_score = models.PositiveIntegerField(null=True, blank=True)
+    avr_score = models.DecimalField(max_digits=5, decimal_places=2, default=0.0, null=True)
     number_tests_passed = models.PositiveIntegerField(null=True, blank=True)
     location = models.CharField(max_length=30, blank=True)
     birth_date = models.DateField(null=True, blank=True, default=datetime.date.today)
+
+    def update_score(self):
+        self.avr_score = self.test_results.values('avr_score').last().get('avr_score')
