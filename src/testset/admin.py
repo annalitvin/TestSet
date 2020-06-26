1f31479820269b747c1c5f27bac5dbde85fba658
annalitvin <litvin_any@ukr.net>
2020-06-27 00:38:02 +0300

    Added Test, Questions, Variants of Questions models in admin panel

--- src/testset/admin.py
+++ src/testset/admin.py
@@ -1,14 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from testset.forms import TestForm, QuestionsInlineFormSet, AnswerInlineFormSet
 from testset.models import Test, Question, Variant
 
 
 class QuestionsInline(admin.TabularInline):
     model = Question
-    fields = ('text',)  # 'num_variant_min_limit')
+    fields = ('text', 'number')  # 'num_variant_min_limit')
     show_change_link = True
-    extra = 1
+    extra = 0
+    formset = QuestionsInlineFormSet
 
 
 class TestAdminModel(admin.ModelAdmin):
@@ -16,20 +18,22 @@ class TestAdminModel(admin.ModelAdmin):
     list_display = ('title', 'description', 'level', 'image')
     list_per_page = 10
     inlines = (QuestionsInline,)
+    form = TestForm
 
 
 class VariantInline(admin.TabularInline):
     model = Variant
-    fields = ('text',)  # 'num_variant_min_limit')
-    show_change_link = True
-    extra = 1
+    fields = ('text', 'is_correct')  # 'num_variant_min_limit')
+    show_change_link = False
+    extra = 0
+    formset = AnswerInlineFormSet
 
 
 class QuestionsAdminModel(admin.ModelAdmin):
-    model = Question
-    fields = ('text',)  # 'num_variant_min_limit')
-    show_change_link = True
-    extra = 1
+    list_display = ('number', 'text', 'description', 'test')
+    list_select_related = ('test',)
+    list_per_page = 10
+    search_fields = ('first_name',)
     inlines = (VariantInline,)
 
 
