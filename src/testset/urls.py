94fe83683b628fab1de58cf82bff4d4c139cc702
annalitvin <litvin_any@ukr.net>
2020-07-04 18:37:56 +0300

    Added session to restore passing the test and fixed LeaderBord template and UserLeaderBoardListView view

--- src/testset/urls.py
+++ src/testset/urls.py
@@ -6,7 +6,7 @@ app_name = 'testset'
 
 urlpatterns = [
     path('list', views.TestListView.as_view(), name='list'),
-    path('<int:pk>/question/<int:seq_nr>', views.TestRunView.as_view(), name='run_step'),
+    path('<int:pk>/next', views.TestRunView.as_view(), name='next'),
     path('start/<int:test_pk>', views.TestStartView.as_view(), name='start'),
     path('leader', views.UserLeaderBoardListView.as_view(), name='leader_bord'),
 ]
