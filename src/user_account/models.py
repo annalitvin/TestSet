5edd3d447eb982f0b5f820c9f9373d1d725d1a7c
annalitvin <litvin_any@ukr.net>
2020-07-04 18:42:39 +0300

    Added analytic metrics for user

--- src/user_account/models.py
+++ src/user_account/models.py
@@ -2,7 +2,7 @@ import datetime
 
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-from django.db.models import Count, Sum
+from django.db.models import Sum
 
 
 class User(AbstractUser):
@@ -15,3 +15,18 @@ class User(AbstractUser):
     def update_score(self):
         self.avr_score = self.test_results.aggregate(points=Sum('avr_score')).get('points', 0.0) / \
                          self.test_results.count()
+
+    def count_passed_tests(self):
+        return self.test_results.filter(is_completed=True).count()
+
+    def total_score(self):
+        return self.avr_score
+
+    def last_run(self):
+        return self.test_results.last().datetime_run
+
+    def percent_success_passed(self):
+        percent_success_passed = sum([user.percent_correct_answers() for user in
+                                      self.test_results.filter(is_completed=True)])
+
+        return round(percent_success_passed, 2)
