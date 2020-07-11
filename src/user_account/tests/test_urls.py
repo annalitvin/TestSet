0e7ac39a3f9fe56cd70f90cd78ce845c3ffd5c78
annalitvin <litvin_any@ukr.net>
2020-07-11 21:43:04 +0300

    Fixed tests for user_account app

--- src/user_account/tests/test_urls.py
+++ src/user_account/tests/test_urls.py
@@ -1,3 +1,4 @@
+from django.core.management import call_command
 from django.urls import reverse
 from django.test import TestCase
 from django.test import Client
@@ -6,12 +7,12 @@ from django.test import Client
 class UrlsAvailabilityTests(TestCase):
 
     def setUp(self):
+        call_command('loaddata', 'tests/fixtures/user_accounts.json', verbosity=0)
+
         self.client = Client()
-        self.client.login(username='anna123', password='nikita160295Angel')
 
     def test_public_url(self):
 
-        self.client.logout()
         urls = [
             (reverse('account:registration'), 'Register new user'),
             (reverse('account:login'), 'Login as a user'),
@@ -24,10 +25,24 @@ class UrlsAvailabilityTests(TestCase):
     def test_private_urls(self):
 
         private_urls = [
+            reverse('account:profile'),
+            reverse('account:logout'),
+
+        ]
+        for url in private_urls:
+            response = self.client.get(url)
+            self.assertRedirects(response, '{}?next={}'.format(reverse('account:login'), url))
+
+    def test_auth_user_urls(self):
+
+        self.client.login(username='anna123', password='nikita160295Angel')
+
+        private_urls = [
             (reverse('account:profile'), 'Edit current user profile'),
             (reverse('account:logout'), 'Logout'),
 
         ]
         for url, content in private_urls:
             response = self.client.get(url)
-            self.assertRedirects(response, '{}?next={}'.format(reverse('account:login'), url))
+            assert response.status_code == 200
+            assert content in response.content.decode()
