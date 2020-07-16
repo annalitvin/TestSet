0afd2b0683f1b656375a76db4428d6f19f45dd3f
annalitvin <litvin_any@ukr.net>
2020-07-16 09:44:30 +0300

    Added save_result signal and remove save_profile signal

--- src/user_account/signals.py
+++ src/user_account/signals.py
@@ -1,17 +1,12 @@
-from django.db.models.signals import post_save, pre_save
+from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 from testset.models import TestResult
-from user_account.models import User
 
 
-@receiver(pre_save, sender=User)
-def save_profile(sender, instance, **kwargs):
-    count_passed_test = instance.count_passed_tests()
-    test_results = instance.test_results.filter(is_completed=True)
-
-    if count_passed_test != 0:
-        percent_success_passed = round((sum([test_result.correct_answers_count() for test_result in test_results]) / \
-                                        sum([test_result.test_question_count() for test_result in test_results]))
-                                       * 100, 2)
-        instance.percent_success = percent_success_passed
+@receiver(post_save, sender=TestResult)
+def save_result(sender, instance, **kwargs):
+    if instance.is_completed:
+        user = instance.user
+        user.update_score()
+        user.save()
