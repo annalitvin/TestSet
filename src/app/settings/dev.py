e0a7221a2b5bc0525d62d0fc34265221c1189f54
annalitvin <litvin_any@ukr.net>
2020-07-25 22:27:52 +0300

    Fixed deploy settings

--- src/app/settings/dev.py
+++ src/app/settings/dev.py
@@ -3,3 +3,7 @@ from app.settings.components.database import *
 from app.settings.components.dev_tools import *
 
 DEBUG = True
+
+ALLOWED_HOSTS = []
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'cdn/static')
