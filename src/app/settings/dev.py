d402fc8df2d2fedaa6a9b9a65c93e3973228ee16
annalitvin <litvin_any@ukr.net>
2020-08-23 16:21:38 +0300

    Fixed dev ALLOWED_HOSTS

--- src/app/settings/dev.py
+++ src/app/settings/dev.py
@@ -1,10 +1,13 @@
 from app.settings.components.base import *
-from app.settings.components.database import *
+#from app.settings.components.database import *
 from app.settings.components.dev_tools import *
 from app.settings.components.email import *
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'cdn/static')
+
+
+CELERY_BROKER_URL = 'amqp://localhost'
